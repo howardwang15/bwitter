6de55cd8709b7e181bdd2a5363bfa36505556c0c
howardwang15 <joy_luck_2000@hotmail.com>
2019-09-27 17:28:53 -0700

    updated GET bweets to return bweet with optinal id query parameter

--- server/app/api/v1/bweet/index.js
+++ server/app/api/v1/bweet/index.js
@@ -11,23 +11,34 @@ admin.initializeApp({
 const db = admin.firestore();
 
 
-router.route('/').get(async (req, res, next) => {
+router.route('/:id?').get(async (req, res, next) => {
+    if (req.params.id) {
+        const doc = await db.collection('bweet').doc(req.params.id).get();
+        const bweet = doc.data();
 
-    const getDataFromDoc = (doc) => {
-        return doc.get().then(doc => {
-            const data = doc.data();
-            data.id = doc.id;
-            return data;
-        });
-    };
+        if (!bweet) {
+            return res.json({ data: []});
+        } else {
+            return res.json({ data: [bweet] });
+        }
 
-    const collection = db.collection('bweet');
-    const documents = await collection.listDocuments();
-    const promises = documents.map(getDataFromDoc);
-
-    const data = await Promise.all(promises);
-
-    return res.json({ data });
+    } else {
+        const getDataFromDoc = (doc) => {
+            return doc.get().then(doc => {
+                const data = doc.data();
+                data.id = doc.id;
+                return data;
+            });
+        };
+    
+        const collection = db.collection('bweet');
+        const documents = await collection.listDocuments();
+        const promises = documents.map(getDataFromDoc);
+    
+        const data = await Promise.all(promises);
+    
+        return res.json({ data });
+    }
 });
 
 module.exports = { router };
